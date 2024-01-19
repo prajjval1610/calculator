a4ad7852092028b4a1c08330afcefb6c6bc22b3a
prajjval kushwaha <119031781+prajjval1610@users.noreply.github.com>
2024-01-19 23:34:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+<script>
+    let result = document.getElementById('result');
+    let expression = '';
+
+    function appendNumber(number) {
+      expression += number
+      }
+      result.value = expression;
+    
+
+    function appendOperator(operator) {
+      expression += operator
+      result.value = expression;
+    }
+
+    function calculate() {
+      try 
+        result.value = eval(expression);
+        expression = '';
+       catch (error) 
+        result.value = 'Error';
+    }
+
+    function clearResult() 
+      expression = '';
+      result.value = '';
+  </script>
